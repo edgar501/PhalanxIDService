878a351b20917775b1115ce42a7568584739240e
Edgar Ademir Morales Pérez <edgar.morales@code-ing.com>
2017-10-10 17:04:23 -0500

    Added static files.

--- PhalanxIDService/settings.py
+++ PhalanxIDService/settings.py
@@ -107,4 +107,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
